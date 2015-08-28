b4895cd4f12d42c7ca9e81cb55f6e39168c4e558
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2015-08-28 11:30:11 +0200

    PIO-81 Fix wrong script name

--- bootstrap.py
+++ bootstrap.py
@@ -4,7 +4,7 @@ import sys
 from os.path import join
 
 source_path = join("src", "main", "python")
-script_path = join("src", "main", "scripts", "aws-cli")
+script_path = join("src", "main", "scripts", "afp-cli")
 
 sys.path.insert(0, source_path)
 exec(open(script_path, 'r').read())
