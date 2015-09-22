de2d1f0ec4039434d8d864df288f3ff7c9f03013
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2015-09-22 19:59:15 +0200

    Release version 1.0.6

--- build.py
+++ build.py
@@ -17,7 +17,7 @@ authors = [Author('Stefan Neben', "stefan.neben@immobilienscout24.de"),
            Author('Enrico Heine', "enrico.heine@immobilienscout24.de"),
            ]
 url = 'https://github.com/ImmobilienScout24/afp-cli'
-version = '1.0.5'
+version = '1.0.6'
 description = open("README.rst").read()
 license = 'Apache License 2.0'
 
