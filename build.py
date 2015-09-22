3767e5dcc6c008cb85caedaf8b5e53b10391de3e
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2015-09-22 19:58:32 +0200

    Python 3 is now officially supported

--- build.py
+++ build.py
@@ -40,6 +40,10 @@ def set_properties(project):
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
          ])
 
     project.set_property('distutils_console_scripts', ['afp=afp_cli.cli:main'])
