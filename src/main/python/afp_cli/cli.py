fb112c89accec9116582eebee92126adeb945041
Michael Kuehne <michael@mkuehne.com>
2015-10-28 10:07:47 +0100

    PIO-118 example url updated

--- src/main/python/afp_cli/cli.py
+++ src/main/python/afp_cli/cli.py
@@ -9,7 +9,7 @@ Options:
   -h --help                Show this.
   --debug                  Activate debug output.
   --user=<username>        The user you want to use.
-  --api-url=<api-url>      The URL of the AFP server (e.g. https://afp.rz.is/afp-api/latest).
+  --api-url=<api-url>      The URL of the AFP server (e.g. https://afp/afp-api/latest).
   --show                   Show credentials instead of opening subshell.
   --export                 Show credentials in an export suitable format.
   --write                  Write credentials to aws credentials file.
