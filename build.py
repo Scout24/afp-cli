363ea2ff8499646eccf84314838d4ee27287313d
Valentin Haenel <valentin@haenel.co>
2016-01-25 14:58:40 +0100

    beautify

--- build.py
+++ build.py
@@ -16,8 +16,7 @@ name = 'afp-cli'
 summary = 'Command line client for AWS federation proxy api'
 authors = [Author('Stefan Neben', "stefan.neben@immobilienscout24.de"),
            Author('Tobias Vollmer', "tobias.vollmer@immobilienscout24.de"),
-           Author(
-               'Stefan Nordhausen', "stefan.nordhausen@immobilienscout24.de"),
+           Author('Stefan Nordhausen', "stefan.nordhausen@immobilienscout24.de"),
            Author('Enrico Heine', "enrico.heine@immobilienscout24.de"),
            Author('Valentin Haenel', "valentin.haenel@immobilienscout24.de"),
            ]
@@ -53,11 +52,10 @@ def set_properties(project):
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
-    ])
+        ])
 
     project.set_property('distutils_console_scripts', ['afp=afp_cli.cli:main'])
-    project.set_property(
-        'distutils_console_scripts', ['afpv2=afp_cli.cliv2:main'])
+    project.set_property('distutils_console_scripts', ['afpv2=afp_cli.cliv2:main'])
     project.set_property('install_dependencies_upgrade', True)
     project.set_property('coverage_exceptions', ['afp_cli.config',
                                                  'afp_cli.cli'])
