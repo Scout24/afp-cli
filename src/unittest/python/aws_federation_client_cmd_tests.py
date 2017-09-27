43816b9156c9aca8211f96749a898eaa6981d6eb
Michael Seiwald <michael.seiwald@scout24.com>
2017-09-27 15:58:08 +0200

    fix unittests to work with requests.Session()

--- src/unittest/python/aws_federation_client_cmd_tests.py
+++ src/unittest/python/aws_federation_client_cmd_tests.py
@@ -11,18 +11,16 @@ class AWSFederationClientCmdTest(TestCase):
     def setUp(self):
         self.api_client = AWSFederationClientCmd()
 
-    @patch("afp_cli.client.requests.get")
-    def test_get_correct_account_and_role_list(self, mock_get):
+    def test_get_correct_account_and_role_list(self):
         expected_result = Mock(text='{"testaccount": ["testrole"]}',
                                status_code=200,
                                reason="Ok")
-        mock_get.return_value = expected_result
-        result = self.api_client.get_account_and_role_list()
-        self.assertEqual(
-            result, {"testaccount": ["testrole"]}, msg='Should be the same')
+        with patch.object(self.api_client.session, 'get', return_value=expected_result):
+            result = self.api_client.get_account_and_role_list()
+            self.assertEqual(
+                result, {"testaccount": ["testrole"]}, msg='Should be the same')
 
-    @patch("afp_cli.client.requests.get")
-    def test_get_correct_aws_credentials(self, mock_get):
+    def test_get_correct_aws_credentials(self):
         expected_result = Mock(
             text='{"Code": "Success", '
             '"AccessKeyId": "testAccessKey", '
@@ -31,12 +29,12 @@ class AWSFederationClientCmdTest(TestCase):
             '"Expiration": "2015-01-01T12:34:56Z"}',
             status_code=200,
             reason="Ok")
-        mock_get.return_value = expected_result
-        result = self.api_client.get_aws_credentials("testaccount", "testrole")
-        self.assertEqual(result, {
-            "AWS_ACCESS_KEY_ID": "testAccessKey",
-            "AWS_SECRET_ACCESS_KEY": "testSecretAccessKey",
-            "AWS_SESSION_TOKEN": "testToken",
-            "AWS_SECURITY_TOKEN": "testToken",
-            "AWS_EXPIRATION_DATE": "2015-01-01T12:34:56Z"},
-            msg='Should be the same')
+        with patch.object(self.api_client.session, 'get', return_value=expected_result):
+            result = self.api_client.get_aws_credentials("testaccount", "testrole")
+            self.assertEqual(result, {
+                "AWS_ACCESS_KEY_ID": "testAccessKey",
+                "AWS_SECRET_ACCESS_KEY": "testSecretAccessKey",
+                "AWS_SESSION_TOKEN": "testToken",
+                "AWS_SECURITY_TOKEN": "testToken",
+                "AWS_EXPIRATION_DATE": "2015-01-01T12:34:56Z"},
+                msg='Should be the same')
